20b58b37ade3f23f45b6d233e6eb9b4fb9a549a8
kimdw <dao@KimDongWookui-MacBookPro.local>
2022-02-10 20:15:35 +0900

    todo list app dev completed

--- TodoApp/src/App.js
+++ TodoApp/src/App.js
@@ -1,14 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+//import AsyncStorage from '@react-native-community/async-storage';
 
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
+
+import todosStorage from './storages/todosStorage';
 
 function App() {
   const today = new Date();
-  
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
+  useEffect(() => {
+    todosStorage
+      .get()
+      .then(setTodos)
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    todosStorage.set(todos).catch(console.error);
+  }, [todos]);
+
+  // useEffect(() => {
+  //   async function load() {
+  //     try{
+  //       const rawTodos = await AsyncStorage.getItem('todos');
+  //       const savedTodos = JSON.parse(rawTodos);
+  //       setTodos(savedTodos);
+  //     } catch (e) {
+  //       console.log('Failed to load todos');
+  //     }
+  //   }
+  //   load();
+  // }, []);
+
+  // useEffect(() => {
+  //   async function save() {
+  //     try {
+  //       await AsyncStorage.setItem('todos', JSON.stringify(todos));
+  //     } catch (e) {
+  //       console.log('Failed to save todos');
+  //     }
+  //   }
+  //   save();
+  // }, [todos]);
+
+  const onInsert = text => {
+    const nextId = todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const todo = {
+      id: nextId,
+      //text: text,
+      text,
+      done: false,
+    };
+
+    setTodos(todos.concat(todo));
+  };
+
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+    setTodos(nextTodos);
+  };
+
+  const onRemove = id => {
+    const nextTodos = todos.filter(todo => todo.id !== id);
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -17,8 +86,8 @@ function App() {
           behavior={Platform.select({ios: 'padding', android: undefined})}
           style={styles.avoid}>
           <DateHead date={today}/>
-          <Empty />
-          <AddTodo />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove}/>}
+          <AddTodo onInsert={onInsert}/>
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
---
+++ TodoApp/src/components/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
+import TodoItem from './TodoItem';
+
+function TodoList({todos, onToggle, onRemove}) {
+    return (
+        <FlatList
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+            style={styles.list}
+            data={todos}
+            renderItem={({item}) => (
+                // <View>
+                //     <Text>{item.text}</Text>
+                // </View>
+                <TodoItem id={item.id} text={item.text} done={item.done} onToggle={onToggle} onRemove={onRemove}/>
+            )}
+            keyExtractor={item => item.id.toString()}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+    },
+    separator: {
+        backgroundColor: '#e0e0e0',
+        height: 1,
+    },
+});
+
+export default TodoList;
