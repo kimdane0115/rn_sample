023f7953a04d7182a633b40e8270e4f15170e067
kimdw <dao@KimDongWookui-MacBookPro.local>
2022-02-18 23:10:59 +0900

    UnderReactNavigation, Material top bottom, useNavigation, useRoute, useFocusEffect commit

---
+++ UnderReactNavigation/src/screens/MainScreen.js
@@ -0,0 +1,150 @@
+import React, {useEffect, useCallback} from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {Text, View, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+const Tab = createMaterialBottomTabNavigator();
+
+//Method 1
+// function OpenDetailButton({onPress}) {
+//     return <Button title = "Detail 1 열기" onPress={onPress}/>;
+// }
+
+//Method 2
+// function OpenDetailButton({navigation}) {
+//     return (
+//         <Button 
+//             title = "Detail 1 열기"
+//             onPress = {() => navigation.push('Detail', {id: 1})}
+//         />
+//     );
+// }
+
+// useNavigation
+function OpenDetailButton() {
+    const navigation = useNavigation();
+
+    return (
+        <Button 
+            title = "Detail 1 열기"
+            onPress = {() => navigation.push('Detail', {id: 1})}
+        />
+    );
+}
+
+function HomeScreen({navigation}) {
+    // useEffect(() => {
+    //     console.log('mounted');
+    //     return () => {
+    //         console.log('unmounted');
+    //     };
+    // }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            console.log('이 화면을 보고 있어요.');
+            return () => {
+                console.log('다른 화면으로 넘어갔어요.');
+            };
+        }, []),
+    );
+
+    return (
+        <View>
+            <Text>Home</Text>
+            {/* <Button
+                title = "Detail 1 열기"
+                onPress={() =>
+                    navigation.push('Detail', {id: 1,})
+                }
+            /> */}
+            {/* <OpenDetailButton onPress={() => navigation.push('Detail', {id: 1})}/> */}
+            {/* <OpenDetailButton navigation={navigation} /> */}
+            <OpenDetailButton />
+        </View>
+    );
+}
+
+function SearchScreen() {
+    return (
+        <View>
+            <Text>Search</Text>
+        </View>
+    );
+}
+
+function NotificationScreen() {
+    return (
+        <View>
+            <Text>Notification</Text>
+        </View>
+    );
+}
+  
+function MessageScreen() {
+    return (
+        <View>
+            <Text>Message</Text>
+        </View>
+    );
+}
+
+function MainScreen() {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            tabBarOptions={{
+                showIcon: true,
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: '홈',
+                    tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
+                    tabBarColor: 'black',
+                    tabBarBadge: 'new',
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{
+                    tabBarLabel: '검색',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="search" color={color} size={24} />
+                    ),
+                    tabBarColor: 'gray',
+                }}
+            />
+            <Tab.Screen
+                name="Notification"
+                component={NotificationScreen}
+                options={{
+                    tabBarLabel: '알림',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="notifications" color={color} size={24} />
+                    ),
+                    tabBarColor: 'green',
+                    tabBarBadge: 30,
+                }}
+            />
+            <Tab.Screen
+                name="Message"
+                component={MessageScreen}
+                options={{
+                    tabBarLabel: '메시지',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="message" color={color} size={24} />
+                    ),
+                    tabBarColor: 'blue',
+                    tabBarBadge: true,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
+export default MainScreen;
