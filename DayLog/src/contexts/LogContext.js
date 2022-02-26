3e9df04af10c8ac1a0179826161dc605b314e65f
kimdw <dao@KimDongWookui-MacBookPro.local>
2022-02-26 20:24:17 +0900

    Animation Sample

---
+++ DayLog/src/contexts/LogContext.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {createContext, useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+const LogContext = createContext();
+
+export function LogContextProvider({children}) {
+    // const [text, setText] = useState('');
+    const [logs, setLogs] = useState([]);
+
+    const onCreate = ({title, body, date}) => {
+        const log = {
+            id: uuidv4(),
+            title,
+            body,
+            date,
+        };
+        setLogs([log, ...logs]);
+    };
+
+    return (
+        // <LogContext.Provider value={{text, setText}}>
+        //     {children}
+        // </LogContext.Provider>
+        <LogContext.Provider value={{logs, onCreate}}>
+            {children}
+        </LogContext.Provider>
+    );
+}
+
+export default LogContext;
